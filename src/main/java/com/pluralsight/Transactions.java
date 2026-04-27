1ee56ccc17b9473c1147ca274c9cc60258670542
killioin007-source <killioin.007@gmail.com>
2026-04-27 10:54:55 -0400

    wrote code in the CSTOR

--- src/main/java/com/pluralsight/Transactions.java
+++ src/main/java/com/pluralsight/Transactions.java
@@ -1,4 +1,44 @@
 package com.pluralsight;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class Transactions {
+    private LocalDate date;
+    private LocalDateTime time;
+    private String description;
+    private String vendor;
+    private double amount;
+
+    public Transactions(LocalDate date, LocalDateTime time, String description, String vendor, double amount) {
+        this.date = date;
+        this.time = time;
+        this.description = description;
+        this.vendor = vendor;
+        this.amount = amount;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public LocalDateTime getTime() {
+        return time;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void allTranscations(){
+        System.out.printf("%-12t %-10t %-30s %-15s %-8f",date,time,description,vendor,amount);
+    }
 }
