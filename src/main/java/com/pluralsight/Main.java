c67cc8190e20149fb6dff7cd84b8650260175ffd
killioin007-source <killioin.007@gmail.com>
2026-04-27 10:13:02 -0400

    created main menu/home screen

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,17 +1,68 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    static Scanner scanner = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
+    public static void main(String[] args) {
+        mainMenu();
+        System.out.println("Thanks for using Bank of Thapa.");
+
+
+    }
+
+    private static void mainMenu() {
+        String homeScreen = """
+                
+                Welcome to Bank of Thapa!
+                ---------------------------
+                
+                What would you like to do today?
+                
+                *) Add Deposit          (Press D)
+                *) Make Payment (Debit) (Press P)
+                *) Ledger               (Press L)
+                *) Exit                 (Press X to Exit)
+                """;
+        boolean running = true;
+
+        do {
+            System.out.println(homeScreen);
+            String userInput = scanner.nextLine();
+
+            switch (userInput) {
+                case "D", "d":
+                    addDeposit();
+                    break;
+                case "p", "P":
+                    makePayment();
+                    break;
+                case "l", "L":
+                    ledger();
+                    break;
+                case "X", "x":
+                    running = false;
+                    break;
+                default:
+                    System.out.println("Are you trying to hack the bank?");
+            }
+        } while (running);
+    }
+
+
+
+
+    private static void ledger() {
+    }
+
+    private static void makePayment() {
+
+    }
+
+    private static void addDeposit() {
     }
 }
